85be1fb640e6452c4d10dad54cf9cc9b026e2cb8
Benson Njuguna <njugunab655@gmail.com>
2023-04-12 18:27:48 +0300

    Post a success message to user

--- src/postScore.js
+++ src/postScore.js
@@ -1,3 +1,18 @@
+const message = document.querySelector('#message');
+
+const fade = () => {
+  let i = 10;
+  message.style.opacity = 1;
+  const inter = setInterval(() => {
+    if (i <= 0) {
+      clearInterval(inter);
+    } else {
+      message.style.opacity = i / 10;
+      i--;
+    }
+  }, 100);
+};
+
 const postScore = async (e) => {
   e.preventDefault();
   const name = e.target.querySelectorAll('#name')[0].value.toLowerCase();
@@ -6,7 +21,7 @@ const postScore = async (e) => {
   const id = await localStorage.getItem('id');
 
   try {
-    fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`, {
+    await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`, {
       method: 'POST',
       body: JSON.stringify({
         user: name,
@@ -16,6 +31,14 @@ const postScore = async (e) => {
         'Content-type': 'application/json; charset=UTF-8',
       },
     });
+    message.textContent = 'Score posted succesfully!';
+    message.style.display = 'block';
+    setTimeout(async () => {
+      await fade();
+      setTimeout(() => {
+        message.style.display = 'none';
+      }, 1000);
+    }, 6000);
   } catch (err) {
     throw new Error(err);
   }
